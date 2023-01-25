f4b2e9dc9f889188dad4feb2b0a07460d07db74f
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-01-25 10:39:10 -0300

    The path in the colored field

--- problem_solving_paradigms/UVa10102.cpp
+++ problem_solving_paradigms/UVa10102.cpp
@@ -4,38 +4,31 @@ using namespace std;
 
 // The path in the colored field. -> UVa10102.
 
-int field[MAX][MAX], input[MAX];
-stack<int> s; stack<int> u;
+char field[MAX][MAX]; int c_field[MAX][MAX];
 
-void decompose(int n){
-    printf("Começou a decompor\n");
-    for(int i = 0; i < n; i++){        
-        while(input[i]){
-            s.push(input[i] % 10);
-            input[i] /= 10;
-        }
+void print_field(int n){
+    for(int i = 0; i < n; i++){
         printf("\n");
+        for(int j = 0; j < n; j++)
+            printf("%d ", c_field[i][j]);
     }
+    printf("\n");
 }
 
-void create_field(int n){
-    printf("Entrou no create_field\n");
-    decompose(n);
-    printf("CREATE FIELD");
-    for(int i = n - 1; i >= 0; i--)
+void create_field (int n){
+    for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++)
-            {field[i][j] = s.top(); s.pop();}
-    printf("Criou.\n");
+            c_field[i][j] = (int) (field[i][j] - '0');  
 }
 
 int get_distance(int i, int j, int k, int n) 
-    {return fabs((k - i) + (n - j));}
+    {return (fabs(k - i) + fabs(n - j));}
 
 int check(int size, int i, int j){
-    int d = 0, min_path = size*size;
+    int d = 0, min_path = size*size; // d = 0 & min_path = 15*15.
     for(int k = 0; k < size; k++)
         for(int n = 0; n < size; n++)
-            if(field[k][n] == 3)
+            if(c_field[k][n] == 3)
                 {d = get_distance(i, j, k, n); min_path = min(min_path, d);} 
     return min_path;
 }
@@ -43,7 +36,7 @@ int check(int size, int i, int j){
 int best_path(int size, int best_distance){
     for(int i = 0; i < size; i++)
         for(int j = 0; j < size; j++)
-            if(field[i][j] == 1) 
+            if(c_field[i][j] == 1) 
                 best_distance = max(check(size, i, j), best_distance); 
     return best_distance;
 }
@@ -52,10 +45,10 @@ int main(){
     int size, best_distance;
     while(scanf("%d", &size) != EOF){
         for(int i = 0; i < size; i++)
-                scanf("%d", &input[i]);
-        printf("Leu todo o input!\n");
-        create_field(size);
-        printf("created field\n");
+            for(int j = 0; j < size; j++)
+                { if(!j) scanf("\n%c", &field[i][j]);
+                  else   scanf("%c", &field[i][j]); }
+        create_field(size); //print_field(size);
         best_distance = 0;  printf("%d\n", best_path(size, best_distance));
     }
     return 0;
