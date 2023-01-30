e39d4f005a450572cc0d71b2e08b4d2826de4d78
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-01-30 10:49:03 -0300

    CD with correct sum without track

--- problem_solving_paradigms/UVa624.cpp
+++ problem_solving_paradigms/UVa624.cpp
@@ -1,44 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define MAX 20
 
-// observation: tracks do not repeat!
-int s[MAX], cd[MAX], n, tracks, best_track = -1, delta;
-bool found;
+//      (CD - 624)
+// ---------------------------------------------------
+int CD[MAX];
+int n, tracks, best_track, delta, id;
+
+void print_answer(int value){
+    printf("sum: %d\n", value);
+}
 
 bool best_option(int sum){
-    if(fabs(sum - best_track) < delta)
-        {delta = fabs(sum - best_track); return true;}
-    return false;
+    if(fabs(sum - n) <= delta)
+        {delta = fabs(sum - n); return true;}   
+    return false; 
 }
 
-bool add_track(int sum, int value){    
-    for(int i = 0; i < tracks; i++)
-       if(value == s[i]) return false; 
-    if(sum + value > n) return false;
-    return true;
+void save_path(int sum){
+    best_track = sum;
+}
+    
+void backtrack(int pos, int sum){ 
+    if(sum > n) return; 
+    for(int i = pos + 1; i < tracks; i++)
+            backtrack(i , sum + CD[i]);
+    if(best_option(sum)) save_path(sum); 
 }
 
-void backtrack(int sum, int count){ 
-    if(sum <= n && best_option(sum)){
-        best_track = sum;
-        printf("Best Track: %d\n", best_track);
-    }
-    for(int i = 0; i < tracks; i++)
-        if(add_track(sum, cd[i]))  
-            {s[count] = cd[i]; backtrack(sum + cd[i], count + 1);}
+void set_parameters(){
+    best_track = -10; delta = 99999; id = -1;
 }
 
 int main(){
-    while(scanf("%d", &n) != EOF){
-        scanf("%d", &tracks);
-        for(int i = 0; i < tracks; i++) scanf("%d", &cd[i]);
-        memset(s, 0, sizeof s);
-        best_track = -1; found = false; delta = 112345;
-        backtrack(0, 0);
-        printf("\n");
-        //print_solution();
-        //printf("sum: %d\n", best_track);
+    while(scanf("%d", &n) != EOF){ 
+        scanf("%d", &tracks); 
+        for(int i = 0; i < tracks; i++)
+            scanf("%d", &CD[i]);
+        set_parameters();
+        backtrack(-1, 0);
+        print_answer(best_track);
     }
     return 0;
 }
