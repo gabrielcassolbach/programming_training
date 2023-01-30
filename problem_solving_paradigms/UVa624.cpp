36bbb041f641474b6f2fc741f68a305c00467f8f
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-01-30 15:28:03 -0300

    CD Solved

--- problem_solving_paradigms/UVa624.cpp
+++ problem_solving_paradigms/UVa624.cpp
@@ -1,36 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define MAX 20
 
-//      (CD - 624)
-// ---------------------------------------------------
-int CD[MAX];
-int n, tracks, best_track, delta, id;
+/*----------------------------------------------*/
+/*                  (CD - 624)                  */
+/*----------------------------------------------*/
+int CD[MAX], n, tracks, best_track, delta, id;
+vector<int> p; vector<int> bp;
 
 void print_answer(int value){
-    printf("sum: %d\n", value);
+    for(int i = 0; i < bp.size(); i++) 
+        printf("%d ", bp[i]);
+    printf("sum:%d\n", value);
 }
 
 bool best_option(int sum){
-    if(fabs(sum - n) <= delta)
-        {delta = fabs(sum - n); return true;}   
-    return false; 
+    if(fabs(sum - n) < delta)
+        {delta = fabs(sum - n); return true;}
+    return false;
 }
 
 void save_path(int sum){
+    while(!bp.empty()) bp.pop_back();
+    for(int i = 0; i < p.size(); i++)
+        bp.push_back(p[i]);
     best_track = sum;
 }
     
 void backtrack(int pos, int sum){ 
-    if(sum > n) return; 
+    if(sum > n) return;
+    if(pos >= 0) p.push_back(CD[pos]);
     for(int i = pos + 1; i < tracks; i++)
             backtrack(i , sum + CD[i]);
-    if(best_option(sum)) save_path(sum); 
+    if(best_option(sum)) save_path(sum);
+    p.pop_back();
 }
 
 void set_parameters(){
     best_track = -10; delta = 99999; id = -1;
+    p.clear(); bp.clear();
 }
 
 int main(){
