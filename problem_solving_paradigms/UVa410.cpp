25c2a5858e75befee884524dae51f1c9c7614394
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-02-01 10:12:53 -0300

    First Greedy Algorithm

--- problem_solving_paradigms/UVa410.cpp
+++ problem_solving_paradigms/UVa410.cpp
@@ -1,64 +1,35 @@
 #include "bits/stdc++.h"
 using namespace std;
-#define MAX 19
+
+#define MAX 10
 // Greedy Algorithm. -> UVa410 - Station Balance.
+// Solution by: https://github.com/milderhc/competitive-programming
 // ----------------------------------------------
-int containers[5][5]; double imbalance;
 
-void set_imbalance(int c, double average){
-    int test_case;
+int mass[MAX]; int cases = 1;
+
+void print_answer(double sum, int c, double diff){
+    printf("SET #%d\n", cases++);
     for(int i = 0; i < c; i++){
-        printf("SET #%d\n", i+1);
         printf(" %d:", i);
-        test_case = 0;
-        for(int j = 0; j < 2; j++){
-            if(containers[i][j]) {
-                test_case += containers[i][j];
-                printf(" %d", containers[i][j]);
-            }
-        }
+        if(mass[i]) printf(" %d", mass[i]);
+        if(mass[2*c - 1 - i]) printf(" %d", mass[2*c - 1 - i]);
         printf("\n");
-        imbalance += ((double)test_case - average);                   
+        diff += fabs(sum - fabs(mass[i] + mass[2*c - 1 - i]));
     }
-    printf("IMBALANCE = %.6lf\n", imbalance);
-    imbalance = 0;
-}
-
-void set_containers(int* weights, int n, int c){
-    int a = 0; int b = n - 1; int value;
-    for(int i = 0; i < c; i++){
-        value = weights[a];
-        for(int j = 0; j < 2; j++){
-            if(value) containers[i][j] = value;
-            value = weights[b];
-        }
-        a++; b--;
-    }
-}
-
-void best_division(int s, int c, int* weights, double average){
-    int n = s; 
-    if(s < (2*c)) n = (s + ((2*c) - s));
-    sort(weights, weights + n);
-    set_containers(weights, n, c);
-    set_imbalance(c, average);
-}
-
-void set_parameters(int* weights){
-    imbalance = 0;
-    memset(weights, 0, sizeof weights); 
-    memset(containers, 0, sizeof containers);
+    printf("IMBALANCE = %.5lf\n\n", diff);
 }
 
 int main(){
-    int s, c, weights[MAX];
-    double average;
+    int c, s;
+    double sum;
     while(scanf("%d", &c) != EOF){
-        scanf(" %d", &s);
-        average = 0;
-        for(int i = 0; i < s; i++) {scanf("%d", &weights[i]); average += weights[i];}
-        set_parameters(weights);
-        best_division(s, c, weights, average/(3.0));
+        sum = 0;
+        scanf(" %d", &s); 
+        for(int i = 0; i < s; i++) {scanf("%d", &mass[i]); sum += mass[i];} 
+        for(int i = s; i < (2*c); i++) mass[i] = 0;
+        sort(mass, mass + (2*c)); 
+        sum = sum / ((double)c); 
+        print_answer(sum, c, 0);
     }
-    return 0;
 }
