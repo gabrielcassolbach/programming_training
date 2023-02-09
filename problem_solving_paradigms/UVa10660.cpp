779e95bf9fc21517de68be5cf29010de7298eb63
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-02-09 17:31:44 -0300

    Falha 10660

---
+++ problem_solving_paradigms/UVa10660.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // First input line: number of test cases.
+    /*
+    {
+        -> The data for each test case appear in sucessive lines.
+        1. number of areas in the city with non null population (n).
+        {
+            for(int i = 0; i < n; i++)
+                leitura de tres números (a, b, c).
+                a: row; b: collumn; c: p: people quantity in that area.
+        }
+
+    }
+    // Output: five numbers indicating the solution.
+    */
+    return 0;
+}
