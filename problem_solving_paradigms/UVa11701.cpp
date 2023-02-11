951c93210dd3d7da236a3c44983b0bb715162e33
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-02-11 15:08:47 -0300

    Cantor

--- problem_solving_paradigms/UVa11701.cpp
+++ problem_solving_paradigms/UVa11701.cpp
@@ -1,41 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define EPS  0.00001
-#define DIFF 0.002
+#define EPS 0.0001
 
-double base_three (double m){
-    double sum = 0;  int digit = 0; double n = m;
-    for(int i = 1; i <= 6 && (abs(n) > EPS); i++){
-        digit = (floor)((n * (pow(10, i))) + 0.001); 
-        sum += (pow(3, -(i)) * digit); 
-        n -= (digit / (pow(10, i)));   
-    }
-    return sum;
+bool number(double n){
+    if(n >= 0 && n <= 1) return true;
+    return false;
 }
 
 bool member(double number){
-    double a = 0; double b = 1; double m = (a + b) / ((double)2);
     int cont = 0;
-    while(cont != 40){
-        cont++;
-        //printf("m: %lf\n", m); printf("p_value: %lf\n", base_three(m));
-        if(fabs(base_three(m) - number) < EPS)
-            return true;
-        if(base_three(m) > number){
-            b = m; 
-            m = (a + b) / ((double) 2);
-        }else{
-            a = m; 
-            m = (a + b) / ((double) 2);
-        }
+    double a = 0, b = 1, size, m1, m2;
+    while(cont != 20){
+        cont++; 
+        size = (b - a) / 3.0;
+        m1 = a + size; m2 = b - size;
+        if((fabs(number - b) <= EPS) || (fabs(number - a) <= EPS)) return true;
+        if(number <= m1) b = m1;
+        else if(number >= m2) a = m2;
+        else return false;
     } 
-    return false;   
+    return false;
 }
 
 int main(){
     double tc;
-    while(scanf("%lf", &tc) != EOF){
+    while(scanf("%lf", &tc) && number(tc)){
         if(member(tc)) printf("MEMBER\n");
         else printf("NON-MEMBER\n");
     }
