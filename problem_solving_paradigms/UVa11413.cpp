6dd15069eb4949aa0d5564318a05c07903c900a2
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-02-28 10:20:00 -0300

    Understanding Conveyor belt

---
+++ problem_solving_paradigms/UVa11413.cpp
@@ -0,0 +1,63 @@
+// Fill the containers -> UVa11413.
+// --------------------------------
+// Solution using the content of the page:
+// https://www.redgreencode.com/binary-search-answer-uva-11413-uva-12032/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1000
+
+int v[MAX];
+
+bool sum_v(int n){
+    int sum = 0;
+    for(int i = 0; i < n; i++)  
+        sum += v[i];
+    return sum;
+}
+
+bool tryCapacity(int n, int m, int maxCapacity){
+    int current_container_size, number_of_containers;
+    current_container_size = 0; 
+    number_of_containers = 0;
+    for(int i = 0; i < n; i++){
+        if(v[i] > maxCapacity) 
+            return false;
+        if((current_container_size + v[i]) > maxCapacity)
+            current_container_size = 0;
+        if(!current_container_size)
+            number_of_containers++;
+        if(number_of_containers > m)
+            return false;
+        current_container_size += v[i];
+    }
+    return true;
+}
+
+void binary_search(int n, int m){
+    int lo, hi, best, mid;
+    lo = 1;
+    //hi = sum_v(n);
+    hi = 91;
+    best = hi + 1;
+    while (lo <= hi){
+        mid = lo + (hi - lo) / 2;
+        printf("lo: %d & hi: %d & mid: %d\n", lo, hi, mid);
+        if(tryCapacity(n, m, mid))
+            {best = mid; hi = --mid;}
+        else 
+            {lo = ++mid;}
+        printf("inside the loop: %d\n", best);
+    }
+    printf("outside the loop: %d\n", best);
+}
+
+int main(){
+    int n, m;   
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) 
+        scanf("%d", &v[i]);
+    binary_search(n, m);
+    return 0;
+}
--- problem_solving_paradigms/UVa12032.cpp
+++ problem_solving_paradigms/UVa12032.cpp
@@ -1,32 +1,38 @@
-// The Monkey and the Oiled Bamboo
+// The Monkey and the Oiled Bamboo -> {UVa12032}
 #include <bits/stdc++.h>
 using namespace std;
 
 #define MAX 112345
 
-void min_k(int tc, int n, int* rungs){
-    int a = 0; int b = n - 1; int m = (a + b) / 2;
-    while(fabs(a - b) != 1){
-        if((rungs[m] - rungs[a]) > (rungs[b] - rungs[m]))
-            {b = m; m = (a + b) / 2;}
-        else
-            {a = m; m = (a + b) / 2;}        
-    }
-    int k = rungs[b] - rungs[a];
-    printf("%d\n", k);
-}   
+/*
+- Input starts with a integer t (t <= 500), denoting the number of test cases.
+Each case starts with a line containing an integer n denoting the number of 
+rungs in the ladder. The next line contains n space separated integers r1, r2, .. rn. 
+denoting the heights of the rungs from the ground.
+
+- observations: 
+ - we can have 10⁵ rungs in the worst case.
+ - each rung can have a value of 10⁷.
+
+- Output:
+- For each test case, print the minimum k value necessary to the person climb up the ladder.
+-> The objective of this problem is to find an algorithm that finds that.
+*/
+
+void min_k (int t, int n, int* v){
+    
+
+}
 
 int main(){
-    int tc, n, rungs[MAX];
-    int cont = 0;
-    scanf("%d", &tc);
-    while(tc--){
-        cont++;
+    int t, n;
+    int v[MAX];
+    scanf("%d", &t);
+    while(t--){
         scanf("%d", &n);
         for(int i = 0; i < n; i++) 
-            scanf("%d", &rungs[i]);
-        printf("Case %d: ", cont);
-        min_k(tc, n, rungs);
+            scanf("%d", &v[i]);
+        min_k(t, n, v);
     }
     return 0;
 } 
