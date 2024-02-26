5be1014b0e6810710e8bac8e57e2b5e8b965a056
gabrielcassolbach <gabrielcassolbach@gmail.com>
2024-02-26 10:12:52 -0300

    o retorno da trip

--- cses-problem-set/Missing Coin Sum.cpp
+++ cses-problem-set/Missing Coin Sum.cpp
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 typedef long long ll;
 
 int main(){
     int n; cin >> n;
     vector<ll> v; v.resize(n);
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++)
         cin >> v[i];
-        
     
+    sort(v.begin(), v.end());
+
+    ll sum = v[0];
+    if(sum != 1)
+        {cout << 1 << endl; return 0;}
+
+    for(int i = 1; i < n; i++){ 
+        if(v[i] > sum + 1)
+            {cout << sum + 1 << endl; return 0;}
+        sum += v[i];
     }
 
-
+    cout << sum + 1 << endl;
     return 0;
 }
