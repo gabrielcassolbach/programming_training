5559d50c0a6e52af838c87665cde76d2054f24b7
gabrielbach <gabrielcassolbach@gmail.com>
2024-07-22 19:05:25 -0300

    caveira!

---
+++ contests/maratonamineira/X/H.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main(){
+
+
+
+	return 0;
+}
--- cses-problem-set/Hotel Queries.cpp
+++ cses-problem-set/Hotel Queries.cpp
@@ -1,46 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define MAX 212345 
+
 typedef long long ll;
 
-#define MAX 212345
-
+vector<ll> h; vector<ll> g;
 ll n, m;
-vector<ll> h, g; 
-ll seg[4*MAX];
+ll seg[4*MAX]; 
 
 void read(){
-	cin >> n >> m;
-	h.resize(n + 3); g.resize(m + 3); 
-	for(int i = 1; i <= n; i++) cin >> h[i]; 
+	cin >> n >> m; 
+	h.resize(n + 10); g.resize(m + 10);
+	for(int i = 1; i <= n; i++) cin >> h[i];
 	for(int i = 1; i <= m; i++) cin >> g[i];
 }
 
-ll build_seg(int p, int l, int r){
-	if(l == r) return seg[p] = h[l];
-	int m = (l+r)/2;
-	return seg[p] = max(build_seg(2*p, l, m), build_seg(2*p + 1, m+1, r));
+ll build_seg(ll p, ll l, ll r){
+	if(l == r) return seg[p] = h[l];	
+	ll m = (l+r)/2;
+	return seg[p] = max(build_seg(2*p, l, m), build_seg(2*p + 1, m + 1, r));
 }
 
-ll query(ll a, ll b, ll p, ll l, ll r) {
-
+ll solve(ll val, ll p, ll l, ll r) {
+	if(l == r) return l;
+	ll m = (l + r)/2;
+	if(seg[2*p] >= val) 
+		return solve(val, 2*p, l, m);
+	else 
+		return solve(val, 2*p + 1, m + 1, r);
 }
 
-void update_seg(){
-	
+ll update(ll index, ll x, ll p, ll l, ll r) {
+	if(index < l or index > r) return seg[p];
+	if(l == r) return h[l] = seg[p] = x;
+	ll m = (l+r)/2;
+	return seg[p] = max(update(index, x, 2*p, l, m), update(index, x, 2*p + 1, m + 1, r));
 }
 
-void answer(){
+void queries(){
+	ll ans = 0;
 	for(int i = 1; i <= m; i++){
-		cout << hotel(1, g[i]) << endl;
-		update_seg();
+		if(g[i] > seg[1]){
+			cout << 0 << " ";
+		}else{			
+			ans = solve(g[i], 1, 1, n);
+			cout << ans << " ";
+			update(ans, h[ans] - g[i], 1, 1, n);	
+		}	
 	}
+	cout << endl;
 }
 
 int main(){
-	read();
-	build_seg(1, 0, n-1);
-        answer();
+	read();	
+	build_seg(1, 1, n);
+	queries();	
 	return 0;
 }
 
