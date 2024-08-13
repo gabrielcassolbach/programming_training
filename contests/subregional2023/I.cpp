02520c91bdd9626ef9b1dfeb9490e4a5f77c58b0
gabrielbach <gabrielcassolbach@gmail.com>
2024-08-13 17:29:11 -0300

    understanding zeros and ones

--- contests/subregional2023/I.cpp
+++ contests/subregional2023/I.cpp
@@ -6,6 +6,17 @@ using namespace std;
 typedef long long ll;
 
 int main() { _
+	ll pref[2] = {1, 0};
 	ll n; cin >> n;
+	ll mask = 0; ll ans = 0; ll cont = 0;
+	while(n--) {
+		ll val; cin >> val;
+		cont += val; 
+		mask = (cont % 2 != 0); 		
+		pref[mask]++;
+		ans += pref[!mask];		
+	}
+	cout << ans << endl;
 	return 0;
 }
+	
