3d83c1768d04d580778109c22454e609f5566c4d
gabrielbach <gabrielcassolbach@gmail.com>
2024-10-03 14:01:29 -0300

    dp approach for two sets ii

--- cses-problem-set/Gray Code.cpp
+++ cses-problem-set/Gray Code.cpp
@@ -8,23 +8,26 @@ typedef long long ll;
 
 vector<string> _a, _b;
 
-void solve (int n, string a, string b) {
-	if(n == 1) {_a.pb(a); _b.pb(b); return;}	
-	solve(n-1, a+"0", b+"1");
-	solve(n-1, a+"1", b+"0");
-}
+/*
+000
+001
+011
+010
+110
+111
+101
+100
+*/
+
 
 int main() {
 	fastio;
 	ll n; cin >> n; 
-	solve(n, "0", "1");
-	for(int i = 0; i < _a.size(); i += 2) {
-				
-
-	
-		cout << _a[i] << '\n';
-	}
-
-	for(auto i = _b.begin(); i != _b.end(); i++) cout << (*i) << '\n';
+	cout << (n >> 1) << '\n';
+	//cout << g(3) << '\n';
 	return 0;
 }
+
+
+
+
--- cses-problem-set/Two Sets II.cpp
+++ cses-problem-set/Two Sets II.cpp
@@ -5,23 +5,52 @@ typedef long long ll;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define ii pair<ll, ll>
+#define MAX 64321
+#define MAX2 512
 
 const ll mod = 1e9 + 7;
-
-ll n; vector<ll> v;
+ll dp[MAX][MAX2];
 
 /*
+62625 = 6.10⁴*5.10² = 3*107
 
-1 2 3 4 5 6 7
+*/
 
-1 
+ll solve(ll n, ll sum, ll setsum, ll it){
+	if(it == n + 1 || sum > setsum) return 0;
+	if(sum == setsum) {return 1;}
+	if(dp[sum][it] != -1) return dp[sum][it];
+	dp[sum][it] = solve(n, sum + it, setsum, it + 1) + solve(n, sum, setsum, it + 1);
+	return dp[sum][it] % mod;
+}
 
- */
+ll soma(ll n){
+	ll sum = 0; n++;
+	while(n--) sum += n;
+	return sum;
+}
 
+void init() {
+	for(int i = 0; i < MAX; i++)
+		for(int j = 0; j < MAX2; j++)
+			dp[i][j] = -1;
+}
+
+ll hasSol(ll n) {
+	ll sum = soma(n);
+	if(sum % 2) return 0; 
+	return (sum/2);
+}
 
 int main() {
 	fastio;
-	
+	init();
+	ll n; cin >> n;
+	ll sol = hasSol(n);
+	if(!sol)
+		cout << 0 << '\n';
+	else
+		cout << solve(n, 0, sol, 1) << '\n';	
 	return 0;
 }
 
