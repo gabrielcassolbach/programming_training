a42ed55d775c249e637f0019978b7ae368b4b79c
gabrielbach <gabrielcassolbach@gmail.com>
2025-01-22 13:58:34 -0300

    embrace pain

---
+++ cses-problem-set/Grid Paths DP.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAX 1001
+ 
+typedef long long ll;
+ 
+const int mod = 1e9 + 7;
+ 
+int n, m[MAX][MAX], dp[MAX][MAX];
+ 
+void printMemory(){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << dp[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+ 
+void init(){
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            dp[i][j] = 0;
+}
+ 
+void read(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            char c; cin >> c;
+            if(c == '.')
+                m[i][j] = 1;
+            else
+                m[i][j] = 0;
+        }
+    }
+}
+ 
+ll moves(){
+    dp[n][n] = m[n][n];
+ 
+    if(!dp[n][n]) return 0;
+ 
+    for(int i = n; i >= 1; i--){
+        for(int j = n; j >= 1; j--){
+            if(i == n && j == n) continue;
+            
+            if(m[i][j] == 0) continue;
+ 
+            ll right = 0; ll down = 0;
+            
+            if(j + 1 <= n && dp[i][j+1])
+                right = dp[i][j+1];
+ 
+            if(i + 1 <= n && dp[i+1][j])
+                down = dp[i+1][j];
+            
+            dp[i][j] = (right + down) % mod;
+        }
+    }
+    return dp[1][1];
+}
+ 
+int main(){
+    read();
+    init();
+    cout << moves() << endl;
+    //printMemory();
+    return 0;
+}
--- cses-problem-set/Grid Paths.cpp
+++ cses-problem-set/Grid Paths.cpp
@@ -1,72 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define MAX 1001
-
 typedef long long ll;
 
-const int mod = 1e9 + 7;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+#define f first
+#define s second
 
-int n, m[MAX][MAX], dp[MAX][MAX];
+string s;
+int m[8][8]; 
+ll ans;  
 
-void printMemory(){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            cout << dp[i][j] << "  ";
-        }
-        cout << endl;
-    }
+char get_pos(int x) {
+	char cord[4] = {'D', 'U', 'L', 'R'};
+	return cord[x];
 }
 
-void init(){
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= n; j++)
-            dp[i][j] = 0;
+pair<int, int> get_cord(char c, int i, int j) {
+	if(c == 'D') return {i+1, j};
+	if(c == 'U') return {i-1, j};	
+	if(c == 'L') return {i, j-1};
+	if(c == 'R') return {i, j+1};
 }
 
-void read(){
-    cin >> n;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            char c; cin >> c;
-            if(c == '.')
-                m[i][j] = 1;
-            else
-                m[i][j] = 0;
-        }
-    }
+int valid(int i, int j) {
+	if(i > 7 || i < 1) return 0; 
+	if(j > 7 || j < 1) return 0; 
+	return 1; 
 }
 
-ll moves(){
-    dp[n][n] = m[n][n];
-
-    if(!dp[n][n]) return 0;
-
-    for(int i = n; i >= 1; i--){
-        for(int j = n; j >= 1; j--){
-            if(i == n && j == n) continue;
-            
-            if(m[i][j] == 0) continue;
-
-            ll right = 0; ll down = 0;
-            
-            if(j + 1 <= n && dp[i][j+1])
-                right = dp[i][j+1];
-
-            if(i + 1 <= n && dp[i+1][j])
-                down = dp[i+1][j];
-            
-            dp[i][j] = (right + down) % mod;
-        }
-    }
-    return dp[1][1];
+void print_matrix() {
+	for(int i = 1; i <= 7; i++){
+		for(int j = 1; j <= 7; j++) {
+			cout << m[i][j];
+		}
+		cout << '\n';
+	}
 }
 
-int main(){
-    read();
-    init();
-    cout << moves() << endl;
-    //printMemory();
-    return 0;
+void find_solutions(int i, int j, int pos) {
+	if(!valid(i, j)) return; 
+	if(pos >= 49) return;
+	print_matrix();  
+	cout << i << " " << j << "\n";
+	if(i == 7 && j == 1 && pos == 48) 
+		{ans++; return;}		
+
+	if(s[pos] != '?') {
+		pair<int, int> a = get_cord(s[pos], i, j);
+		m[a.f][a.s] = 1; 	
+		find_solutions(a.f, a.s, pos+1); 
+	}else{
+		for(int x = 0; x < 4; x++){
+			char c = get_pos(x); pair<int, int> a = get_cord(c, i, j); 	
+			if(valid(a.f, a.s) && !m[a.f][a.s]){
+				m[a.f][a.s] = 1; 
+				find_solutions(a.f, a.s, pos + 1);	
+				m[a.f][a.s] = 0; 
+			}
+		}	
+	}	
 }
+
+void init() {
+	for(int i = 0; i < 8; i++)
+		for(int j = 0; j < 8; j++)
+			m[i][j] = 0; 
+}
+
+int main() {
+	fastio; 
+	cin >> s; cout << s << '\n'; 
+	init(); 
+	ans = 0;  
+	find_solutions(1, 1, 0);
+	cout << ans << '\n';	
+	return 0;
+}
+
+
