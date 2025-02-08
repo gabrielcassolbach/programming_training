54283eeecf53899f7ebedf62bffec7cd2220de2f
gabrielbach <gabrielcassolbach@gmail.com>
2025-02-08 16:20:11 -0300

    'simple' backtracking

--- cses-problem-set/Grid Paths.cpp
+++ cses-problem-set/Grid Paths.cpp
@@ -3,78 +3,84 @@ using namespace std;
 
 typedef long long ll;
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
-#define f first
-#define s second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); 
 
 string s;
-int m[8][8]; 
-ll ans;  
+ll ans; 
+int dr[] = {-1, 0, 1, 0};
+int dc[] = {0, 1, 0, -1}; 
+char pos[] = {'U', 'R', 'D', 'L'}; 
+int m[9][9]; // 7x7 grid plus border.  
 
-char get_pos(int x) {
-	char cord[4] = {'D', 'U', 'L', 'R'};
-	return cord[x];
+int f_dir(char c) {
+	int id = 0;
+	for(int i = 0; i < 4; i++)
+		if(c == pos[i]) 
+			id = i;
+	return id;  	
 }
 
-pair<int, int> get_cord(char c, int i, int j) {
-	if(c == 'D') return {i+1, j};
-	if(c == 'U') return {i-1, j};	
-	if(c == 'L') return {i, j-1};
-	if(c == 'R') return {i, j+1};
+int out_grid(int x, int y) {
+	if(x <= 0 || y <= 0) return 1; 
+	if(x >= 8 || y >= 8) return 1; 
+	return 0; 
 }
 
-int valid(int i, int j) {
-	if(i > 7 || i < 1) return 0; 
-	if(j > 7 || j < 1) return 0; 
-	return 1; 
+int split_grid(int x, int y) {
+	int l = m[x+dr[3]][y+dc[3]];
+	int r = m[x+dr[1]][y+dc[1]];
+	int d = m[x+dr[2]][y+dc[2]];
+	int u = m[x+dr[0]][y+dc[0]]; 
+
+	if(!r && !l && u && d)
+		return 1; 
+	if(!u && !d && r && l)
+		return 1;
+	return 0; 
 }
 
-void print_matrix() {
-	for(int i = 1; i <= 7; i++){
-		for(int j = 1; j <= 7; j++) {
-			cout << m[i][j];
-		}
-		cout << '\n';
+int stop(int i, int x, int y) {
+	if(out_grid(x, y) || m[x][y]) return 1; 
+	if(i != 48 && x == 7 && y == 1) return 1; 
+	if((i == 48 && x != 7) || i > 48) return 1;
+	if(split_grid(x, y)) return 1;
+	return 0;  
+}
+
+void solve(int i, int x, int y) {
+	if(stop(i, x, y)) return; 
+	if(i == 48 && x == 7 && y == 1) {ans++; return;}	
+	
+	m[x][y] = 1; 
+
+	if(s[i] == '?') {
+		for(int j = 0; j < 4; j++)
+			solve(i+1, x + dr[j], y + dc[j]); 
+	} else {
+		int j = f_dir(s[i]); 
+		solve(i+1, x + dr[j], y + dc[j]); 
 	}
+
+	m[x][y] = 0; 
 }
 
-void find_solutions(int i, int j, int pos) {
-	if(!valid(i, j)) return; 
-	if(pos >= 49) return;
-	print_matrix();  
-	cout << i << " " << j << "\n";
-	if(i == 7 && j == 1 && pos == 48) 
-		{ans++; return;}		
-
-	if(s[pos] != '?') {
-		pair<int, int> a = get_cord(s[pos], i, j);
-		m[a.f][a.s] = 1; 	
-		find_solutions(a.f, a.s, pos+1); 
-	}else{
-		for(int x = 0; x < 4; x++){
-			char c = get_pos(x); pair<int, int> a = get_cord(c, i, j); 	
-			if(valid(a.f, a.s) && !m[a.f][a.s]){
-				m[a.f][a.s] = 1; 
-				find_solutions(a.f, a.s, pos + 1);	
-				m[a.f][a.s] = 0; 
-			}
-		}	
-	}	
-}
-
-void init() {
-	for(int i = 0; i < 8; i++)
-		for(int j = 0; j < 8; j++)
+void set_var() {
+	for(int i = 0; i < 9; i++)
+		for(int j = 0; j < 9; j++)
 			m[i][j] = 0; 
+	for(int i = 0; i < 9; i++) {
+		m[i][0] = m[0][i] = 1;
+		m[8][i] = m[i][8] = 1; 
+	}
+	ans = 0; 
 }
 
 int main() {
-	fastio; 
-	cin >> s; cout << s << '\n'; 
-	init(); 
-	ans = 0;  
-	find_solutions(1, 1, 0);
-	cout << ans << '\n';	
+	fastio;
+	set_var();
+	cin >> s;
+	solve(0, 1, 1);
+	cout << ans << '\n';
 	return 0;
 }
 
