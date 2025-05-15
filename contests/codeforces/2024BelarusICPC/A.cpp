9b5ac61e181a7366170ff428e2f2c340dc9f0347
Gabriel Cassol Bach <gbach@MMB039579CWB.local>
2025-05-15 11:51:10 -0300

    ta saindo

--- contests/codeforces/2024BelarusICPC/A.cpp
+++ contests/codeforces/2024BelarusICPC/A.cpp
@@ -2,41 +2,68 @@
 using namespace std;
 
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+#define f first
+#define s second 
+#define pb push_back
 
-vector<int> v; 
-map<int, int> hash_m; // {value, key}.  
+typedef long long ll;
+
+const int MAX = 1e9 + 7; 
+
+vector<int> v;
+map<int, int> hash_m, hash_print; // {value, key}.  
 int n, k;
 
-void answer(int ans) {
-  /*cout << "answer: "; 
-  cout << ans << '\n';*/
+void answer(int ans, int q) {
+  if (ans == n) 
+    {cout << -1 << '\n'; return;}
+  cout << ans << '\n';
+  
+  int it = 1;
+  while(it <= n) {
+    hash_print[it++] = 1;
+    first_v += q;
+  }
+    
+  for(int i = 1; i <= n; i++){
+    if(hash_print[v[i]] == 0)
+
+  }
+
+  cout << '\n';
 }
 
 void solve() {
-  int a = 0; int b = v[n] - v[1];
-  int ans = n; int debug = 0;
-  
-  /*while(a <= b){
-    int c = (a + b)/2;*/
-    int c = 2; // razão. -> será testada em cada iteração da busca binária.
-    int cnt = 1, aux_k = 0;
+  int a = v[1], b = v[n];
+  int ans = n; int q = MAX;
 
+  while(a <= b){
+    int c = (a + b)/2;
+    int cnt = 1, aux_k = 0;
+    //cout << "a: " << a << " b: " << b << '\n'; 
+    //cout << "c: " << c << '\n';
     auto it = hash_m.find(v[cnt] + c);
+    aux_k += (it == hash_m.end()) ? (n - cnt) : 0; 
+
     while(it != hash_m.end()) {  
-      cout << "cnt: " << cnt << '\n';
       aux_k += hash_m[v[cnt] + c] - cnt - 1;
       cnt = hash_m[v[cnt] + c];
       it = hash_m.find(v[cnt] + c);
+      aux_k += (it == hash_m.end()) ? (n - cnt) : 0; 
     }
-    cout << "testing: " <<  aux_k << '\n';
-    cout << "aqui: " << hash_m[cnt] << '\n';
-    aux_k += n - hash_m[cnt];
 
-  
-    cout << "aux_k: " << aux_k << '\n';
-  //}
-  
-  answer(ans > k ? -1 : ans);
+    if(aux_k <= k) {
+      ans = min(ans, aux_k);
+      q = min(q, c);
+      a = c + 1;
+    }else{
+      b = c - 1;
+    }
+
+    //cout << "aux_k: " << aux_k << '\n';
+  }
+
+  answer(ans, q);
 }
 
 int main() {
