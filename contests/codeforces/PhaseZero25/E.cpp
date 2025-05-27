f73f53dde00d40088ed5cd27df6f1349eb921ef5
Gabriel Bach <gabrielcassolbach@gmail.com>
2025-05-27 19:16:38 -0300

    dale

--- contests/codeforces/PhaseZero25/E.cpp
+++ contests/codeforces/PhaseZero25/E.cpp
@@ -3,39 +3,50 @@ using namespace std;
 
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0);
 #define pb push_back
-#define MAX 1123456789
+#define MAX 112345
 
 typedef long long ll; 
 
-vector<int> primos, prime_factors, stop_cond; 
+vector<ll> primos, prime_factors, jumps;
 vector<ll> d;
-int y, k; 
+ll y, k; 
 
-// ok!
 void crivo(){
-    vector<int> primos_ax; 
-    primos_ax.resize((int) sqrt(MAX), 1);
-    for(int i = 2; i < (int) sqrt(MAX); i++){
+    vector<ll> primos_ax; 
+    primos_ax.resize((ll) MAX, 1);
+    for(ll i = 2; i < (ll) MAX; i++){
         if(!primos_ax[i]) continue;
-        for(int j = i+i; j < (int) sqrt(MAX); j+=i)
+        for(ll j = i+i; j < (ll) MAX; j+=i)
             primos_ax[j] = 0; 
     }
-    for(int i = 2; i <= (int) sqrt(MAX); i++)   
+    for(ll i = 2; i < (ll) MAX; i++)   
         if(primos_ax[i])
             primos.pb(i);
 }
 
 
+ll get_jumps(ll it, ll k){
+    if(it == prime_factors.size() || k < (prime_factors[it] - 1))
+        return (ll)(k+1)*jumps[it];
+    return get_jumps(it + 1, k - (prime_factors[it] - 1));
+}
+
+void get_factors(ll n){
+    for(int i = 0; i < (ll) primos.size() && n > 1 && primos[i] <= n; i++)
+        while(!(n % primos[i])) prime_factors.pb(primos[i]), n /= primos[i];
+    if(n != 1) prime_factors.pb(n);
+}
+
 ll solve(){
-    cin >> y >> k; 
-    int i = 0; ll ans = 2; k--;  
-    if(k == 0) return ans; 
-
+    cin >> y >> k;
+  
+    get_factors(y);
     
-   
+    jumps.resize(prime_factors.size() + 1, 1);
+    for(int i = 0; i < prime_factors.size(); i++)
+        jumps[i+1] = prime_factors[i]*jumps[i];
 
-
-    return 1;
+    return get_jumps(0, k);
 }
 
 int main() {
--- contests/codeforces/PhaseZero25/H.cpp
+++ contests/codeforces/PhaseZero25/H.cpp
@@ -1,8 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+#define pb push_back
+
+typedef long long ll;
+
+vector<ll> get_bits(ll x) {
+    vector<ll> bits;
+    while(x > 0){
+        if(x % 2) 
+            x -= 1, bits.pb(1ll);
+        else
+            bits.pb(0);
+        x /= (ll)2;       
+    }
+    reverse(bits.begin(), bits.end());
+    return bits;
+}
+
+pair<ll, ll> solve(ll x){
+    vector<ll> bits = get_bits(x);
+    ll ans = 0, half_ans = 0; 
+    
+    for(ll i = 0; i < bits.size()/2 + (bits.size() % 2); i++)
+        ans += bits[i]*((ll)1 << (ll)(bits.size() - 1 - i));    
+    
+    half_ans = ans;
+    for(ll i = 0; i < bits.size()/2; i++)
+        ans += bits[bits.size()/2 - 1 - i]*((ll)1 << (ll)bits.size()/2 - 1 - i); 
+    
+    return {ans, half_ans};
+}
+
 int main() {
+    fastio; 
+    vector<ll> bits; 
+    ll x; cin >> x; 
 
+    ll ans, half_ans;
+    auto value = solve(x);
+   
+    ans = value.first; half_ans = value.second;
 
+    if(ans > x){
+        bits.clear();
+        ans = solve(half_ans-1).first;
+    }
+    
+    cout << ans << '\n';
     return 0;
 }
