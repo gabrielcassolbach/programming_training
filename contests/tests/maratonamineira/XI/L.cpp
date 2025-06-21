98a555501ec6ce91cf01641c1e05f725183bfda0
Gabriel Bach <gabrielcassolbach@gmail.com>
2025-06-21 14:01:12 -0300

    spes nostra

--- contests/tests/maratonamineira/XI/L.cpp
+++ contests/tests/maratonamineira/XI/L.cpp
@@ -1,21 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 int n, q;
-vector<int> v;
+vector<vector<int>> tree; 
+vector<int> te, ts, h, func, ft; 
 
-void read(){
-	cin >> n >> q;
-	v.resize(n + 10);
-	for(int i = 2; i <= n; i++)
-		cin >> v[i];
+int t = 0;
+
+void dfs(int node, int height){
+	h[node] = height;
+	te[node] = t++; 
+	for(auto u : tree[node])
+		if(te[u] == -1)
+			dfs(u, height + 1);	
+	ts[node] = t;
+}
+
+bool comp(int f1, int f2){
+	return h[f1] > h[f2];
+}
+
+int query(int t_ent, int t_said){
+	int sum = 0;
+	for(int i = t_said; i > 0; i -= (i & -i)) sum += ft[i];
+	for(int i = t_ent; i > 0; i -= (i & -i)) sum -= ft[i];
+	return sum;
+}
+
+void update(int t_ent, int val){
+	for(int i = t_ent+1; i <= n; i += (i & -i)) ft[i] += val;
+}
+
+int solve(){
+	vector<int> v; 
+	sort(func.begin(), func.end(), comp);
+	int ans = 0;
+	for(int i = 0; i < func.size(); i++){
+		if(query(te[func[i]], ts[func[i]]) == 0)
+			ans++, update(te[func[i]], 1), v.push_back(te[func[i]]);
+	}
+	for(int i = 0; i < v.size(); i++) 
+		update(v[i], -1);
+	return ans; 
 }
 
 int main(){
-	read();
+	ios_base::sync_with_stdio(0);
+	cin >> n >> q; 
+	tree.resize(n);
+	for(int i = 1; i < n; i++){
+		int val; cin >> val;
+		tree[val-1].push_back(i);
+	}
+	te.resize(n, -1); ts.resize(n, -1);  h.resize(n);
+	dfs(0, 0);
 	
+	ft.resize(n + 1, 0);
+	while(q--){
+		int val; cin >> val;
+		func.resize(val);
+		for(int i = 0; i < val; i++)
+			cin >> func[i], func[i]--;
+		cout << solve() << '\n';
+	}
 
 	return 0;
 }
--- cses-problem-set/Removal Game.cpp
+++ cses-problem-set/Removal Game.cpp
@@ -6,13 +6,10 @@ typedef long long ll;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define MAX 5123
 
+const ll inf = -1e5;
+
 ll n; vector<ll> v;
 ll dp[MAX][MAX];
-
-/*
-4
-4 5 1 3
-*/ 
 
 void read() {
 	cin >> n; v.resize(n + 10);
@@ -26,8 +23,13 @@ ll choice(ll a, ll b, ll p) {
 }
 
 ll solve(ll a, ll b, ll p) {
+	//cout << "a: " << a << " b: " << b << '\n';
 	if(a == b) return v[a];
-	if(dp[a][b] != -1) return dp[a][b];
+	if(dp[a][b] != -2) {
+		//cout << dp[a][b] << " ";
+		return dp[a][b];
+	}
+
 	if(b - a == 1) return choice(a, b, p);
 	if(p) 
 		dp[a][b] = max(solve(a+1, b, p^1)+v[a], solve(a,b-1,p^1)+v[b]);
@@ -37,7 +39,8 @@ ll solve(ll a, ll b, ll p) {
 }
 
 int main() {
-	fastio; read(); memset(dp, -1, sizeof dp);
+	fastio; read(); memset(dp, -2, sizeof dp);
+	cout << dp[1][1] << '\n';
 	cout << solve(1, n, 1) << '\n';	
 	return 0;
 }
