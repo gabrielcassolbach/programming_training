3d23ec9cf75a900e7491f35cbedf0bd3ed3d58d5
Gabriel Cassol Bach <gabrielcassolbach@gmail.com>
2025-08-20 09:25:02 -0300

    ARMADA

--- contests/tests/subregional2018/e.cpp
+++ contests/tests/subregional2018/e.cpp
@@ -4,19 +4,23 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     string a, b; 
-    cin >> a; cin >> b; 
+    cin >> a; cin >> b;
+    
+    int lim = (int) a.size() -  (int) b.size();
+    int ans = 0;  
 
+    for(int shift = 0; shift <= lim; shift++){
+        bool ok = true;
 
-    for(int i = 0; i < (int) a.size(); i++){
+        for(int j = 0; j < (int) b.size(); j++)
+            if(a[shift + j] == b[j])
+                ok = false;
         
-
-
+        if(ok) 
+            ans++;
     }
 
-
-
-
-
-
+    cout << ans << "\n";
+    
     return 0;
 }
